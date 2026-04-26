b3762cfb9d0515c38a12fbdf8c95d609633c97c7
muhammad hamza khan <mh9813951@gmail.com>
2026-04-26 09:36:36 +0500

    practicing

---
+++ list.py
@@ -0,0 +1,41 @@
+number = [2,4,6,3,8,10,2,6]
+
+# j = len(number)-1
+# i = 0
+
+# # Reverse an array method 01
+# while i < j:
+#     result = number[i]
+#     number[i] = number[j]
+#     number[j] = result
+#     i += 1
+#     j -= 1
+# print(number)
+
+
+# # Reverse an array method 01
+# arr = []
+# while j >= 0:
+#     arr.append(number[j])
+#     j -= 1
+# print(arr)
+
+
+
+
+#sum of all elements of list
+# for x in number:
+#     result += x
+# print(result)
+
+
+# max element in list
+# for i in range(1, len(number)):
+#     if number[i] > number[j]:
+#         result = number[i]
+#     else:
+#         result = number[j]
+#     j +=1
+# print(result)
+
+
---
+++ remove.duplicates.py
@@ -0,0 +1,4 @@
+number = [2,4,6,3,8,10,2,6]
+set_value = set(number)
+number = list(set_value)
+print(number)
